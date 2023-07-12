065c031bf6de177687f7aa8a90643d2a58bb7851
chenxiuyuan02 <chenxiuyuan02@meituan.com>
2023-07-12 09:56:26 +0800

    add model

--- users/models.py
+++ users/models.py
@@ -5,11 +5,11 @@ from django.dispatch import receiver
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
-    gender = models.CharField(max_length = 11, default = '', blank=True)
-    address = models.CharField(max_length = 100, default = '', blank=True)
-    city = models.CharField(max_length = 100, default = '', blank=True)
-    state = models.CharField(max_length = 100, default = '', blank=True)
-    czip = models.CharField(max_length = 100, default = '', blank=True)
+    gender = models.CharField(max_length=11, default='', blank=True)
+    address = models.CharField(max_length=100, default='', blank=True)
+    city = models.CharField(max_length=100, default='', blank=True)
+    state = models.CharField(max_length=100, default='', blank=True)
+    czip = models.CharField(max_length=100, default='', blank=True)
 
     class Meta:
         verbose_name = 'User Profile'
