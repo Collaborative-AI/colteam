cd9d5868146613058772a81b895ded4b8e15aad1
YUE <yue@YUEdeMacBook-Pro-2.local>
2023-08-04 01:23:20 -0400

    add login, signup feature, error handling

--- my-react-app/src/components/login.component.js
+++ my-react-app/src/components/login.component.js
@@ -1,35 +1,87 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 export default class Login extends Component {
-  render() {
-    return (
-      <form>
-        <h3>Sign In</h3>
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
-        </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
-        </div>
-        
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </div>
-        <p className="forgot-password text-right">
-          <a href="#">Forgot password?</a>
-        </p>
-      </form>
-    )
+  constructor(props){
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
   }
-}
+
+  // Update component state when input values change
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  // Handle form submission
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    // Build data object to be sent to the backend
+    const data = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    // Send POST request to Django backend's login API
+    axios
+      .post('http://localhost:8000/api/login/', data)
+      .then((response) => {
+        // Actions to perform after successful login, e.g., saving token
+        console.log(response.data);
+      })
+      .catch((error) => {
+        // Handle errors
+        if (error.response) {
+          console.error('Status Code:', error.response.status);
+          console.error('Data:', error.response.data);
+          console.error('Response Header:', error.response.headers);
+        } else {
+          console.error('Error:', error.message);
+        }
+      });
+    }
+
+    render() {
+      return (
+        <form onSubmit={this.handleFormSubmit}>
+          <h3>Sign In</h3>
+          <div className="mb-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="forgot-password text-right">
+            <a href="#">Forgot password?</a>
+            {/* to do: add forgot password handle */}
+          </p>
+        </form>
+      )
+    }
+};
--- my-react-app/src/components/signup.component.js
+++ my-react-app/src/components/signup.component.js
@@ -1,8 +1,57 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 export default class SignUp extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+    };
+  }
+
+  // Update component state when input values change
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    // Construct the data object to send to the backend
+    const data = {
+      first_name: this.state.firstName,
+      last_name: this.state.lastName,
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    // Send a POST request to the Django backend's registration API
+    axios
+      .post('http://localhost:8000/api/register/', data)
+      .then((response) => {
+        // Handle successful registration, e.g., show success message
+        console.log(response.data);
+      })
+      .catch((error) => {
+        // Handle errors
+        if (error.response) {
+          console.error('Status Code:', error.response.status);
+          console.error('Data:', error.response.data);
+          console.error('Response Header:', error.response.headers);
+        } else {
+          console.error('Error:', error.message);
+        }
+      });
+  };
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleFormSubmit}>
         <h3>Sign Up</h3>
         <div className="mb-3">
           <label>First name</label>
@@ -10,11 +59,21 @@ export default class SignUp extends Component {
             type="text"
             className="form-control"
             placeholder="First name"
+            name="firstName"
+            value={this.state.firstName}
+            onChange={this.handleInputChange}
           />
         </div>
         <div className="mb-3">
           <label>Last name</label>
-          <input type="text" className="form-control" placeholder="Last name" />
+          <input 
+            type="text" 
+            className="form-control" 
+            placeholder="Last name" 
+            name="lastName"
+            value={this.state.lastName}
+            onChange={this.handleInputChange}
+          />
         </div>
         <div className="mb-3">
           <label>Email address</label>
@@ -22,6 +81,9 @@ export default class SignUp extends Component {
             type="email"
             className="form-control"
             placeholder="Enter email"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleInputChange}
           />
         </div>
         <div className="mb-3">
@@ -30,6 +92,9 @@ export default class SignUp extends Component {
             type="password"
             className="form-control"
             placeholder="Enter password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleInputChange}
           />
         </div>
         <div className="d-grid">
