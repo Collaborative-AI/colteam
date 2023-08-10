98161c0d554e2656928be8f51a5d22d7e8e0be0c
elainelpr <1224037466lpr@gmail.com>
2023-08-09 23:32:37 -0400

    database

--- users/models.py
+++ users/models.py
@@ -11,3 +11,7 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return "{}".format(self.user.__str__())
+
+class User(models.Model):
+    userName = models.CharField(max_length = 20)
+    passWord = models.CharField(max_length = 100)
