34df013f17501f1b88a9ad492a47d94aaabd7ab1
YUE <yue@YUEdeMacBook-Pro-2.local>
2023-08-11 23:49:07 -0400

    Revert "Merge branch 'sasa_back' into frontend_react"
    This reverts commit da9eef8d68eb00a817bfe829779905985bbd2639, reversing
    changes made to 7c0b3f3c03d9cef2c190860bfd460b80aa4624f6.

--- users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-# Register your models here.
-# from .models import UserProfile
-from .models import CustomUserProfile
-
-# admin.site.register(UserProfile)
-admin.site.register(CustomUserProfile)
