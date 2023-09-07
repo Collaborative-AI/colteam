8db1c001d33d9aba6f87257185aa7182c06b993e
yxx137 <xy137@duke.edu>
2023-09-07 00:42:59 -0700

    show_project add token

--- my-react-app/src/components/show_project.component.js
+++ my-react-app/src/components/show_project.component.js
@@ -1,29 +1,32 @@
-import React, { useState, useEffect, useContext } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect, useContext } from 'react'
+import axios from 'axios'
 import AuthContext from './AuthProvider.component'
 import { useNavigate } from 'react-router-dom'
 
-function ShowProject() {
-  const [projects, setProjects] = useState([]);
+function ShowProject () {
+  const [projects, setProjects] = useState([])
   const { auth } = useContext(AuthContext)
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   useEffect(() => {
     // URL
-    const backendURL = 'http://localhost:8000';
+    const backendURL = 'http://localhost:8000'
     axios.get(`${backendURL}/projects/view_all/`, {
       params: {
         id: auth.username
+      },
+      headers: {
+        Authorization: "Bearer " + auth.accessToken
       }
     })
       .then(response => {
-        setProjects(response.data);
-        console.log("test:", response.data[0].title);
+        setProjects(response.data)
+        console.log("test:", response.data[0].title)
       })
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
+      .catch(error => console.error('Error fetching data:', error))
+  }, [])
 
   const handleReturn = () => {
-    navigate('/profile');
+    navigate('/profile')
   }
 
   return (
@@ -37,7 +40,7 @@ function ShowProject() {
       </ul>
       <button onClick={handleReturn}>Return</button>
     </div>
-  );
+  )
 }
 
-export default ShowProject;
+export default ShowProject
