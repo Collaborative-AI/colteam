9bf4ba8bb411bd634a1f8af56e2be0f4991463dc
yxx137 <xy137@duke.edu>
2023-09-11 23:39:34 -0700

    my project delete

--- my-react-app/src/components/project/show_project.component.js
+++ my-react-app/src/components/project/show_project.component.js
@@ -3,9 +3,10 @@ import axios from 'axios'
 import AuthContext from '../AuthProvider.component'
 import { useNavigate, Link } from 'react-router-dom'
 
-function ShowProject() {
+
+function ShowProject () {
   const [projects, setProjects] = useState([])
-  const { auth } = useContext(AuthContext)
+  const { auth, setAuth } = useContext(AuthContext)
   const navigate = useNavigate()
   useEffect(() => {
     // URL
@@ -29,14 +30,45 @@ function ShowProject() {
     navigate('/profile')
   }
 
+  function handleDelteProject (projectID) {
+    const data = {
+      id: projectID
+    }
+    const config = {
+      headers: {
+        Authorization: "Bearer " + auth.accessToken
+      }
+    }
+    axios.post(`http://localhost:8000/projects/delete/`, data, config)
+      .then((response) => {
+        console.log(response.data)
+        navigate('/show_project')
+        setProjects(projects.filter(project => project.id !== projectID))
+      })
+      .catch((error) => {
+        setAuth({ success: false })
+        if (error.response) {
+          console.error('Status Code:', error.response.status)
+
+          console.error('Data:', error.response.data)
+          console.error('Response Header:', error.response.headers)
+        } else {
+          console.error('Error:', error.message)
+        }
+      })
+  }
+
   return (
     <div>
       <h1>Projects List</h1>
       <ul>
         {projects.map(project => (
-          <li key={project.id}>
-          <Link to={`/project/${project.id}`}>{project.title}</Link>
-        </li>
+          <li className="show-project-item" key={project.id}>
+            <Link to={`/project/${project.id}`}>{project.title}</Link>
+            <div className="project-button-container">
+              <button className="project-button" onClick={() => handleDelteProject(project.id)}>delete project</button>
+            </div>
+          </li>
         ))}
       </ul>
       <button onClick={handleReturn}>Return</button>
