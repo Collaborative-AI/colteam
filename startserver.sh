e36b98e10118a2c49cf79bfc889052a64e3a782a
Xiuyuan Chen <xc202@duke.edu>
2023-10-23 18:26:59 -0400

    redis package

---
+++ startserver.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+pip install celery
+pip install djongo
+pip install django-redis
+pip install django-cors-headers
+pip install channels==3.0.4
+pip install channels_redis==3.3.1
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+res="$?"
+while [ "$res" != "0" ]
+do
+    sleep 3;
+    python3 manage.py migrate
+    res="$?"
+done
+python3 manage.py runserver 0.0.0.0:8000
+
+celery -A colteam worker --loglevel=info
