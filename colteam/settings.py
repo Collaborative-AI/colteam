13ff0fe4a2370ece6bfa1455988d341bad299c06
Xiuyuan Chen <xc202@duke.edu>
2023-10-23 10:55:15 -0400

    消息中心

--- colteam/settings.py
+++ colteam/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'projects.apps.ProjectsConfig',
+    'messages.apps.MessagesConfig'
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
