ba6c01204cf7e31a6594567e19048d985a24d67c
yxx137 <xy137@duke.edu>
2023-12-24 00:32:36 -0800

    chatBox details

--- my-react-app/src/components/messages/chatBox.js
+++ my-react-app/src/components/messages/chatBox.js
@@ -1,98 +1,75 @@
 
-// import { useEffect, useContext, useState } from 'react'
-// import AuthContext from '../AuthProvider.component'
-// import axios from 'axios'
+import { useEffect, useContext, useState } from 'react'
+import AuthContext from '../AuthProvider.component'
+import axios from 'axios'
+import './chatBox.scss'
 
-// const CharBox = () => {
-//   const { auth } = useContext(AuthContext)
+const CharBox = () => {
+  const { auth } = useContext(AuthContext)
 
-//   const [chatHistory, setChatHistory] = useState({
-//     list: [{}], //data: {id, message, date}
-//     count: 0  //total chat counts
-//   })
+  const [chatHistory, setChatHistory] = useState({
+    list: [{}], //data: {id, message, date}
+    count: 0  //total chat counts
+  })
 
-//   //load chst history
-//   useEffect(() => {
-//     console.log(`Bearer ${auth.accessToken}`)
-//     axios
-//       .get("http://localhost:8000/forum/get_thread_by_id/", {
-//         headers: {
-//           Authorization: `Bearer ${auth.accessToken}`
-//         },
-//         params: {
-//           t: new Date().getTime()
-//         }
-//       })
-//       .then((response) => {
-//         console.log("chat history get")
-//         console.log(response.data)
-//         //TODO: set chat hisoty
-//         // setChatHistory({
-
-//         // })
-//       })
-//       .catch((error) => console.error('Error fetching char history:', error))
-//   }, [])
-
-//   return (
-//     <div>
-
-//       chatbox
-//       <div></div>
-//     </div>
-//   )
-
-// }
-
-// export default CharBox
-
-
-import React, { useState } from 'react'
-
-const ChatHistory = ({ messages }) => (
-  <div className="chat-history">
-    {messages.map((message, index) => (
-      <p key={index}>{message}</p>
-    ))}
-  </div>
-)
-
-const MessageInput = ({ onSendMessage }) => {
   const [input, setInput] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     if (input.trim()) {
-      onSendMessage(input)
+      //TODO: update chat history to serve
+      setChatHistory({
+        list: [...chatHistory.list, { id: "", message: input, date: "" }],
+        count: chatHistory.count + 1
+      })
       setInput('')
     }
   }
 
-  return (
-    <form className="message-input" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button type="submit">Send</button>
-    </form>
-  )
-}
+  //load chst history
+  useEffect(() => {
+    console.log(`Bearer ${auth.accessToken}`)
+    axios
+      .get("http://localhost:8000/forum/get_thread_by_id/", {
+        headers: {
+          Authorization: `Bearer ${auth.accessToken}`
+        },
+        params: {
+          t: new Date().getTime()
+        }
+      })
+      .then((response) => {
+        console.log("chat history get")
+        console.log(response.data)
+        //TODO: set chat hisoty
+        // setChatHistory({
 
-const ChatBox = () => {
-  const [messages, setMessages] = useState([])
-
-  const sendMessage = (newMessage) => {
-    setMessages([...messages, newMessage])
-  }
+        // })
+      })
+      .catch((error) => console.error('Error fetching char history:', error))
+  }, [])
 
   return (
     <div className="chat-box">
-      <ChatHistory messages={messages} />
-      <MessageInput onSendMessage={sendMessage} />
+      <div className="chat-history">
+        {chatHistory.list.map(({ message }, index) => (
+          <p key={index}>{message}</p>
+        ))}
+      </div>
+      <form className="message-input" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        >
+        </input>
+        <button type="submit"> send </button>
+      </form>
     </div>
   )
+
 }
 
-export default ChatBox
+export default CharBox
+
+
