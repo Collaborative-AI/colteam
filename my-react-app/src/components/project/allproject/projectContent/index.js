76d1fd02978e8a60b92822b2ba8cc79ed89c60e4
yxx137 <xy137@duke.edu>
2024-01-06 23:44:39 -0800

    show all projects

--- my-react-app/src/components/project/allproject/projectContent/index.js
+++ my-react-app/src/components/project/allproject/projectContent/index.js
@@ -29,10 +29,15 @@ function AllProject () {
         headers: {
           Authorization: `Bearer ${auth.accessToken}`
         }
-      }, { params })
+      })
       .then((response) => {
-        console.log(response)
+        console.log("project data from backend")
+        console.log(response.data)
         //projectList(render current project list, and set total amount projects)
+        setProjectList({
+          list: response.data.results,
+          count: response.data.count
+        })
       })
       .catch((error) => console.error('Error fetching all projects data:', error))
   }, [params])
@@ -54,11 +59,25 @@ function AllProject () {
     {
       title: 'categories',
       dataIndex: 'categories',
-      render: data => <Tag color="green">categories</Tag>
+      // render: data => <Tag color="green">categories</Tag>
     },
     {
       title: 'post_date',
-      dataIndex: 'post_date'
+      dataIndex: 'post_date',
+      render: (text, { post_date }, index) => {
+        const indexOfDot = post_date.indexOf(".")
+        return (<p>{post_date.substring(0, indexOfDot).replace("T", " ")}</p>)
+      },
+      width: 220
+    },
+    {
+      title: 'end_date',
+      dataIndex: 'end_date',
+      width: 220,
+      render: (text, { end_date }, index) => {
+        const indexOfDot = end_date.indexOf(".")
+        return (<p>{end_date.substring(0, indexOfDot).replace("T", " ")}</p>)
+      }
     },
     {
       title: 'website',
@@ -85,10 +104,22 @@ function AllProject () {
   const data1 = [
     {
       id: 17,
-      title: "hello",
+      title: "22",
       post_date: "2023-11-06T21:04:59.885000",
       description: "",
       categories: "1",
+      website: "https://www.baidu.com",
+      email: "wang3ys@163.com",
+      qualification: "",
+      owner: 115,
+      group_member: null
+    },
+    {
+      id: 27,
+      title: "11",
+      post_date: "2024-11-06T21:04:59.885000",
+      description: "",
+      categories: "5",
       website: "https://www.baidu.com",
       email: "wang3ys@163.com",
       qualification: "",
@@ -99,20 +130,20 @@ function AllProject () {
 
 
 
-  // const data2 = [
-  //   {
-  //     id: 27,
-  //     title: "hello22222",
-  //     post_date: "2024-11-06T21:04:59.885000",
-  //     description: "",
-  //     categories: "5",
-  //     website: "https://www.baidu.com",
-  //     email: "wang3ys@163.com",
-  //     qualification: "",
-  //     owner: 115,
-  //     group_member: null
-  //   }
-  // ]
+  const data2 = [
+    {
+      id: 27,
+      title: "hello22222",
+      post_date: "2024-11-06T21:04:59.885000",
+      description: "",
+      categories: "5",
+      website: "https://www.baidu.com",
+      email: "wang3ys@163.com",
+      qualification: "",
+      owner: 115,
+      group_member: null
+    }
+  ]
 
   const data3 = [
     {
@@ -129,15 +160,15 @@ function AllProject () {
     }
   ]
 
-  const data = [{ data1 }, { data3 }]
-  console.log(data)
+  // const data = [data1, data2]
+  // console.log(data)
 
   return (
-    <Card title={`Total projects: count`}>
+    <Card title={`Total projects: ${projectList.count}`}>
       <Table
         rowKey="id"
         columns={columns}
-        dataSource={data}
+        dataSource={projectList.list}
       />
     </Card>
   )
