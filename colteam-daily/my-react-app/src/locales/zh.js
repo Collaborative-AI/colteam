974ac2963b447ebd018bca6ffa71320672504d58
dem123456789 <diao_em@hotmail.com>
2025-02-19 18:23:25 +0800

    merge with daily

--- colteam-daily/my-react-app/src/locales/zh.js
+++
@@ -1,7 +0,0 @@
-export default {
-    hello: `你好`,
-    projectSearch: '搜索你的项目',
-    projectCreate: '创建项目',
-    projectTop: '热门项目/最受好评：',
-    sort: '排序规则'
-  };
--- colteam-develop/users/permissions.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import Permission
-
-
-class UserPermissions(Permission):
-    class Meta:
-        permissions = [
-            ("can_do_something", "Can do something"),
-            ("can_view_something", "Can view something"),
-        ]
